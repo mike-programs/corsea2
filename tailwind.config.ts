f2e5b98d2aea996e9f0fd63606a1153fc2062d98
Michael Mathew <michaelmathew34@gmail.com>
2024-03-27 18:20:58 +0100

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'hero': "url('/images/backdrop.png')",
+        'newsletterBackground': "url('/images/newsletter-background.png')"
       },
     },
   },
